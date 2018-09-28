9eac55a545a29ef5692966ed39498ef91b1b5484
marccespedes <43566746+marccespedes@users.noreply.github.com>
2018-09-28 13:42:55 +0200

    feat: Implemented funtion factorial
    Implemented the code of  factorial function through iterative solution.

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-int factorial(){
-return 0;
+int factorial(int n){
+int i;
+int res = 0;
+for (size_t i = 0; i < n; i++{
+    res = res *  i;
+  /* n*/
+}
+return res;
 }
 
 int int main(int argc, char const *argv[]) {
