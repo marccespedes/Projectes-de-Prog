e16dcd1b78cce8b716c882e0baaf1ee23c88d5b9
marccespedes <43566746+marccespedes@users.noreply.github.com>
2018-09-28 13:46:16 +0200

    fix: Resolving problems related to factorial always returning 0
    Iterator and result both equalled 0

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 int factorial(int n){
-int i;
-int res = 0;
-for (size_t i = 0; i < n; i++{
+  int i;
+  int res = 0;
+
+  for (i = 1; i < n; i++{
     res = res *  i;
   /* n*/
-}
-return res;
+  }
+  return res;
 }
 
 int int main(int argc, char const *argv[]) {
